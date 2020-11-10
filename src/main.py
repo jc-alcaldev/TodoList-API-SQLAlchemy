bb8de7d788dac07f93da724d949e16be23e0d6f4
jc-alcaldev <carlos_juan1812@hotmail.com>
2020-11-10 18:02:55 +0000

    Hecho el get,post

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Tasks
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,37 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/todos/user/<user_route>', methods=['GET'])
+def handle_hello(user_route):
+    username = User.get_user(user_route)
+    user_content = username
+    return jsonify(user_content), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route("/todos/user/<user_route>", methods=["POST"])
+def create_user(user_route):
+    new_user = User(username=user_route)
+    new_user.add_user()
+    return jsonify(new_user.serialize()), 201
 
-    return jsonify(response_body), 200
+@app.route("/todos/user/<user_route>/list", methods=["POST"])
+def create_task(user_route):
+    body = request.get_json()
+    new_task = Tasks(user_name=user_route, task_text=body["task_text"], task_done=body["task_done"])
+    new_task.add_task()
+    return jsonify(new_task.serialize())
+
+# @app.route("/todos/user/<user_route>/list/<int:task_id>", methods=["PUT"])
+# def update_task(user_route, task_id):
+#     body = request.get_json()
+#     updated_task = Tasks(id=body["task_id"], user_name=user_route, task_text=body["task_text"], task_done=body["task_done"])
+#     updated_task.update_task(task_id)
+#     return jsonify(updated_task.serialize())
+
+@app.route("/todos/user/<user_route>", methods=["DELETE"])
+def delete_user(user_route):
+    user_to_delete = User(username=user_route)
+    user_to_delete.delete_user()
+    return "User deleted", 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,60 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= "user"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    tasks_relationship = db.relationship("Tasks", lazy = True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"User: {self.username}"
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "username": self.username,
+            "tasks": self.tasks_relationship
         }
+
+    def add_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def get_user(cls, username):
+        user = User.query.filter_by(username = username).first() # devuelve el primero que encuentra
+        return user.serialize()
+
+class Tasks(db.Model):
+    __tablename__ = "task"
+    id = db.Column(db.Integer, primary_key = True)
+    user_name = db.Column(db.String(120), ForeignKey("user.username"))
+    task_text = db.Column(db.String(250))
+    task_done = db.Column(db.Boolean(False))
+
+    def __repr__(self):
+        return f"task: {self.task_text}, done: {self.task_done}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_name": self.user_name,
+            "task_text": self.task_text,
+            "task_done": self.task_done
+        }
+
+    def add_task(self):
+        db.session.add(self)
+        db.session.commit()
+
+    # def update_task(self, new_done):
+    #     task = Tasks.query.get(self)
+    #     task.task_done = new_done
+    #     db.session.commit()
